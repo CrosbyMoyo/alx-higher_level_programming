8abffecd296e17e389a70ffef844fbd1701ac986
CrosbyMoyo <blessingbmoyo@gmail.com>
2022-06-03 08:43:55 +0200

    updates

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for count in range(1, 101):
-        if count % 3 == 0 and count % 5 == 0:
+    for i in range(1, 101):
+        if i % 15 == 0:
             print("FizzBuzz ", end="")
-        elif count % 3 == 0:
-            print("Fizz ", end="")
-        elif count % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
         else:
-            print('%d ' % (count), end="")
+            print("{:d} ".format(i), end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
-        number = number % 10
-    else:
-        aux = number * -1
-        number = aux % 10
-    print(number, end="")
-    return number
+    exe = 0
+    if number < 0:
+        number *= -1
+    exe = 1
+    last = number % 10
+    if exe == 1:
+        number *= -1
+    print("{:d}".format(last), end="")
+    return last
